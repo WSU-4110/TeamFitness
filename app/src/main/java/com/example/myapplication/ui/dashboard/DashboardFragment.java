9ff66364d9d9bdd6509fa787d110c27027b49e00
DiegoZ-Official <152233772+DiegoZ-Official@users.noreply.github.com>
2024-10-02 15:09:54 -0400

    Update Oct 2 3pm (#34)
    * UseCaseDiagram
    
    * UseCaseDiagram
    
    * Update Untitled Diagram.drawio
    
    * Update Untitled Diagram.drawio
    
    * Pull Request (#33)
    
    * Initial branch commit
    
    * pull request 1 (#32)
    
    * Noah Test
    
    * Noah Test
    
    * Diego Test Commit
    
    ---------
    
    Co-authored-by: Noah Wardiya <hh0890@wayne.edu>
    Co-authored-by: Diego Z <diezam42@gmail.com>
    Co-authored-by: DiegoZ-Official <152233772+DiegoZ-Official@users.noreply.github.com>
    Co-authored-by: Ikemba15 <ikemba15@gmail.com>
    Co-authored-by: 1102noah <wardiyan1@yahoo.com>
    
    * - Changed home page to tracker page; Changed notification page to progress page
    
    - Added Post.java & PostsAdapter.java; used to implement post feature on dashboard/feed page
    
    - Used RecycleView to display posts on dashboard/feed page
    
    - Added default image to every post, implement Glide library for optimized loading of images
    
    - Posts can currently display title, info/description, and image
    
    ---------
    
    Co-authored-by: Noah Wardiya <hh0890@wayne.edu>
    Co-authored-by: Diego Z <diezam42@gmail.com>
    Co-authored-by: DiegoZ-Official <152233772+DiegoZ-Official@users.noreply.github.com>
    Co-authored-by: Ikemba15 <ikemba15@gmail.com>
    Co-authored-by: 1102noah <wardiyan1@yahoo.com>
    
    ---------
    
    Co-authored-by: 1102noah <wardiyan1@yahoo.com>
    Co-authored-by: Gino Reinke <ginpreinke@gmail.com>
    Co-authored-by: Noah Wardiya <hh0890@wayne.edu>
    Co-authored-by: Diego Z <diezam42@gmail.com>
    Co-authored-by: Ikemba15 <ikemba15@gmail.com>

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.dashboard;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,26 +9,85 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDashboardBinding;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.ui.Post;
+import com.example.myapplication.ui.PostsAdapter;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
-private FragmentDashboardBinding binding;
+    private FragmentDashboardBinding binding;
+    private RecyclerView mRecyclerView;
+    private ArrayList<Post> mPostsData;
+    private PostsAdapter mAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
-    binding = FragmentDashboardBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // Initialize the RecyclerView.
+        mRecyclerView = root.findViewById(R.id.recyclerView);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Initialize the ArrayList that will contain the data.
+        mPostsData = new ArrayList<>();
+
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new PostsAdapter(getContext(), mPostsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Get the data.
+        initializeData();
+
         return root;
     }
 
-@Override
+    /**
+     * Initialize the posts data from resources.
+     */
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] postsList = getResources()
+                .getStringArray(R.array.posts_titles);
+        String[] postsInfo = getResources()
+                .getStringArray(R.array.posts_info);
+
+        // Clear the existing data (to avoid duplication).
+        mPostsData.clear();
+        TypedArray postsImageResources =
+                getResources().obtainTypedArray(R.array.posts_images);
+
+        // Create the ArrayList of Posts objects with titles and
+        // information about each post.
+        for(int i=0;i<postsList.length;i++){
+            mPostsData.add(new Post(postsList[i],postsInfo[i],
+                    postsImageResources.getResourceId(i,0)));
+        }
+
+        // Clean up the data in the typed array once you have created the Post data ArrayList:
+        postsImageResources.recycle();
+
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
--- app/src/main/java/com/example/myapplication/ui/progress/ProgressViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/progress/ProgressViewModel.java
@@ -1,16 +1,16 @@
-package com.example.myapplication.ui.home;
+package com.example.myapplication.ui.progress;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ProgressViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ProgressViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is notifications fragment");
     }
 
     public LiveData<String> getText() {
