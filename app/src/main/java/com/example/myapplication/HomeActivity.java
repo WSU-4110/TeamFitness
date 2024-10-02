6963945d300325e6eb4e9a47a4ff123082d13f95
Gino Reinke <ginpreinke@gmail.com>
2024-10-02 19:28:05 -0400

    Gino pull request Account button (#36)
    * Initial branch commit
    
    * pull request 1 (#32)
    
    * Noah Test
    
    * Noah Test
    
    * Diego Test Commit
    
    ---------
    
    Co-authored-by: Noah Wardiya <hh0890@wayne.edu>
    Co-authored-by: Diego Z <diezam42@gmail.com>
    Co-authored-by: DiegoZ-Official <152233772+DiegoZ-Official@users.noreply.github.com>
    Co-authored-by: Ikemba15 <ikemba15@gmail.com>
    Co-authored-by: 1102noah <wardiyan1@yahoo.com>
    
    * - Changed home page to tracker page; Changed notification page to progress page
    
    - Added Post.java & PostsAdapter.java; used to implement post feature on dashboard/feed page
    
    - Used RecycleView to display posts on dashboard/feed page
    
    - Added default image to every post, implement Glide library for optimized loading of images
    
    - Posts can currently display title, info/description, and image
    
    * - Created top_menu.xml to display Accounts in top right
    
    - Added Account page; Added explicit intent to start page from other pages
    
    ---------
    
    Co-authored-by: Noah Wardiya <hh0890@wayne.edu>
    Co-authored-by: Diego Z <diezam42@gmail.com>
    Co-authored-by: DiegoZ-Official <152233772+DiegoZ-Official@users.noreply.github.com>
    Co-authored-by: Ikemba15 <ikemba15@gmail.com>
    Co-authored-by: 1102noah <wardiyan1@yahoo.com>

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,18 +14,21 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.myapplication.databinding.ActivityHomeBinding;
 
 import androidx.appcompat.widget.Toolbar;
+import android.widget.Toast;
+
 
 public class HomeActivity extends AppCompatActivity {
 
-private ActivityHomeBinding binding;
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_home);
 
-     binding = ActivityHomeBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // Find the toolbar and set it as the ActionBar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -36,6 +43,24 @@ private ActivityHomeBinding binding;
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_account) {
+            Intent intent = new Intent(HomeActivity.this, AccountActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
 }
