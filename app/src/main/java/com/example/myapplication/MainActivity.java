d2bf3812c60512bc0cd5b85c80695b67f2604f38
1102noah <wardiyan1@yahoo.com>
2024-10-03 18:43:38 -0400

    FireBase (#47)
    * Connecting and Testing Firebase Database
    
    * Removing test code for DB
    
    ---------
    
    Co-authored-by: Noah Wardiya <hh0890@wayne.edu>
    Co-authored-by: Gino Reinke <ginpreinke@gmail.com>

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
