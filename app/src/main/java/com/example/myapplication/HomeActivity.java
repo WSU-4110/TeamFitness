98e2669641d3a3a8758ca607c13e59e5af118d6d
Gino Reinke <ginpreinke@gmail.com>
2024-10-17 10:53:16 -0400

    Ikemba accounts page pull request (#49)
    * profile added
    
    * profile added
    
    * working profile connected to picture no error.
    
    * actually working now on API 34 and below
    
    * actually working now on API 34 and below
    
    ---------
    
    Co-authored-by: Ikemba Ajaero <ikemba15@gmail.com>

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,48 +1,22 @@
 package com.example.myapplication;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.example.myapplication.databinding.ActivityHomeBinding;
-
 import androidx.appcompat.widget.Toolbar;
-import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_home);
-
-
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Find the toolbar and set it as the ActionBar
+        setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_tracker, R.id.navigation_dashboard, R.id.navigation_progress)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 
     @Override
@@ -54,12 +28,13 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
+
         if (itemId == R.id.action_account) {
-            Intent intent = new Intent(HomeActivity.this, AccountActivity.class);
+            Intent intent = new Intent(HomeActivity.this, Profile.class);
             startActivity(intent);
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
-
-
     }
 }
