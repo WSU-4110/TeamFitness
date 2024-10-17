4094a799cb6481916a569b82e20747063a90b1d4
Noah Wardiya <hh0890@wayne.edu>
2024-10-17 13:37:38 -0400

    Broken Registration

--- app/src/main/java/com/example/myapplication/SignUpPage.java
+++ app/src/main/java/com/example/myapplication/SignUpPage.java
@@ -3,24 +3,52 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.widget.Toast;
 
-import com.google.android.gms.auth.api.phone.SmsRetriever;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 
 public class SignUpPage extends AppCompatActivity {
 
-    // Initialize Button to go back to log in
+    // Initialize Button to go back to log in and to register
     Button backToLogIn;
+    Button registered;
+
+    /*
+     Initializing the email, password, and confirmPassword to
+     turn them to strings
+    */
+    EditText email;
+    EditText password;
+    EditText confirmPassword;
+
+    public boolean emailCheck(String email){
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean passwordCheck(String password){
+        String passwordRegex = "^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>])(?=.*[0-9]).{8,}$";
+        Pattern pattern = Pattern.compile(passwordRegex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public boolean passwordSame(String password1, String password2){
+        if(Objects.equals(password1, password2)){
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +56,8 @@ public class SignUpPage extends AppCompatActivity {
         setContentView(R.layout.sign_up);
 
         backToLogIn = findViewById(R.id.loginButton);
+        registered = findViewById(R.id.registerButton);
+
 
         backToLogIn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -39,6 +69,73 @@ public class SignUpPage extends AppCompatActivity {
                 finish();
             }
         });
-    }
 
+/*
+        registered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Getting the user inputs
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                String userConfirmPassword = confirmPassword.getText().toString();
+
+                Log.d("DEBUG", "Email: " + userEmail);
+                Log.d("DEBUG", "Password: " + userPassword);
+                Log.d("DEBUG", "Confirm Password: " + userConfirmPassword);
+
+                if (emailCheck(userEmail) && passwordCheck(userPassword)
+                        && passwordSame(userPassword, userConfirmPassword)) {
+
+                    Log.d("DEBUG", "Valid email and password. Starting HomeActivity...");
+                    Intent loggedIn = new Intent(SignUpPage.this, HomeActivity.class);
+                    startActivity(loggedIn);
+                    finish();
+                } else {
+                    if (!emailCheck(userEmail)) {
+                        Toast.makeText(SignUpPage.this, "Invalid Email Format. EX: john@gmail.com",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (!passwordCheck(userPassword)) {
+                        Toast.makeText(SignUpPage.this, "Password must have a minimum length of 8, 1 special character, a capital letter, and a number.",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SignUpPage.this, "Passwords must match.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+*/
+
+        registered.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Getting the user inputs
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                String userConfirmPassword = confirmPassword.getText().toString();
+
+                // Checks the users inputs
+                if (emailCheck(userEmail) && passwordCheck(userPassword)
+                        && passwordSame(userPassword, userConfirmPassword)) {
+                        Intent intent = new Intent(SignUpPage.this, HomeActivity.class);
+                        startActivity(intent);
+                        finish();
+                }
+                // If the user enters an incorrect field it will let them know
+                else {
+                    if (!emailCheck(userEmail)) {
+                        Toast.makeText(SignUpPage.this, "Invalid Email Format. EX: john@gmail.com",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (!passwordCheck(userPassword)) {
+                        Toast.makeText(SignUpPage.this, "Password must have a minimum length of 8, 1 special character, a capital letter, and a number."
+                                , Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SignUpPage.this, "Passwords must match.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            };
+        });
+    }
 }
